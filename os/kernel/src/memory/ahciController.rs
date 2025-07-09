3b505cf8c314088f96a3ed4a94cadcfb6c0c661c
FabianGremme <fagre101@hhu.de>
2025-07-09 22:35:02 +0000

    planung von komponenten für identify device

--- os/kernel/src/memory/ahciController.rs
+++ os/kernel/src/memory/ahciController.rs
@@ -186,6 +186,167 @@ struct HbaCommandTable {
     reserved: [u8;48],
 }
 
+//hier sollten alle Geräteinfos stehen:
+/*
+struct DeviceInfo {
+        uint16_t config;                /* lots of obsolete bit flags */
+        uint16_t cyls;                  /* obsolete */
+        uint16_t reserved2;             /* special config */
+        uint16_t heads;                 /* "physical" heads */
+        uint16_t track_bytes;           /* unformatted bytes per track */
+        uint16_t bytesPerSector;        /* unformatted bytes per sector */
+        uint16_t sectors;               /* "physical" sectors per track */
+        uint16_t vendor0;               /* vendor unique */
+        uint16_t vendor1;               /* vendor unique */
+        uint16_t vendor2;               /* vendor unique */
+        uint8_t serialNumber[20];       /* 0 = not specified */
+        uint16_t buf_type;
+        uint16_t buf_size;              /* 512 byte increments; 0 = not specified */
+        uint16_t ecc_bytes;             /* for r/w long cmds; 0 = not specified */
+        uint8_t firmwareRevision[8];    /* 0 = not specified */
+        uint8_t Port[40];              /* 0 = not specified */
+        uint16_t multi_count;           /* Multiple Count */
+        uint16_t dword_io;              /* 0=not_implemented; 1=implemented */
+        uint16_t capability1;           /* vendor unique */
+        uint16_t capability2;           /* bits 0:DMA 1:LBA 2:IORDYsw 3:IORDYsup word: 50 */
+        uint8_t vendor5;                /* vendor unique */
+        uint8_t tPIO;                   /* 0 = slow, 1 = medium, 2 = fast */
+        uint8_t vendor6;                /* vendor unique */
+        uint8_t tDMA;                   /* 0 = slow, 1 = medium, 2 = fast */
+        uint16_t field_valid;           /* bits 0:cur_ok 1:eide_ok */
+        uint16_t cur_cyls;              /* logical cylinders */
+        uint16_t cur_heads;             /* logical heads word 55 */
+        uint16_t cur_sectors;           /* logical sectors per track */
+        uint16_t cur_capacity0;         /* logical total sectors on drive */
+        uint16_t cur_capacity1;         /* (2 words, misaligned int)     */
+        uint8_t multsect;               /* current multiple sector count */
+        uint8_t multsect_valid;         /* when (bit0==1) multsect is ok */
+        uint32_t lbaCapacity;           /* total number of sectors */
+        uint16_t dma_1word;             /* single-word dma info */
+        uint16_t dma_mword;             /* multiple-word dma info */
+        uint16_t eide_pio_modes;        /* bits 0:mode3 1:mode4 */
+        uint16_t eide_dma_min;          /* min mword dma cycle time (ns) */
+        uint16_t eide_dma_time;         /* recommended mword dma cycle time (ns) */
+        uint16_t eide_pio;              /* min cycle time (ns), no IORDY */
+        uint16_t eide_pio_iordy;        /* min cycle time (ns), with IORDY */
+        uint16_t words69_70[2];         /* reserved words 69-70 */
+        uint16_t words71_74[4];         /* reserved words 71-74 */
+        uint16_t queue_depth;
+        uint16_t sata_capability;       /* SATA Capabilities word 76 */
+        uint16_t sata_additional;       /* Additional Capabilities */
+        uint16_t sata_supported;        /* SATA Features supported */
+        uint16_t features_enabled;      /* SATA features enabled */
+        uint16_t major_rev_num;         /* Major rev number word 80 */
+        uint16_t minor_rev_num;         /* Minor revision number */
+        uint16_t command_set_1;         /* bits 0: Smart, 1: Security, 2: Removable, 3: PM */
+        uint16_t command_set_2;         /* bits 14:Smart Enabled 13:0 zero */
+        uint16_t cfsse;                 /* command set-feature supported extensions */
+        uint16_t cfs_enable_1;          /* command set-feature enabled */
+        uint16_t cfs_enable_2;          /* command set-feature enabled */
+        uint16_t csf_default;           /* command set-feature default */
+        uint16_t dma_ultra;
+        uint16_t word89;                /* reserved (word 89) */
+        uint16_t word90;                /* reserved (word 90) */
+        uint16_t CurAPMvalues;          /* current APM values */
+        uint16_t word92;                /* reserved (word 92) */
+        uint16_t comreset;              /* should be cleared to 0 */
+        uint16_t accoustic;             /*  accoustic management */
+        uint16_t min_req_sz;            /* Stream minimum required size */
+        uint16_t transfer_time_dma;     /* Streaming Transfer Time-DMA */
+        uint16_t access_latency;        /* Streaming access latency-DMA & PIO WORD 97*/
+        uint32_t perf_granularity;      /* Streaming performance granularity */
+        uint32_t total_usr_sectors[2];  /* Total number of user addressable sectors */
+        uint16_t transfer_time_pio;     /* Streaming Transfer time PIO */
+        uint16_t reserved105;           /* Word 105 */
+        uint16_t sector_sz;             /* Physical Sector size / Logical sector size */
+        uint16_t inter_seek_delay;      /* In microseconds */
+        uint16_t words108_116[9];       /* Reserved */
+        uint32_t words_per_sector;      /* words per logical sectors */
+        uint16_t supported_settings;    /* continued from words 82-84 */
+        uint16_t command_set_3;         /* continued from words 85-87 */
+        uint16_t words121_126[6];       /* reserved words 121-126 */
+        uint16_t word127;               /* reserved (word 127) */
+        uint16_t security_status;       /* device lock function
+                                         * 15:9   reserved
+                                         * 8   security level 1:max 0:high
+                                         * 7:6   reserved
+                                         * 5   enhanced erase
+                                         * 4   expire
+                                         * 3   frozen
+                                         * 2   locked
+                                         * 1   en/disabled
+                                         * 0   capability */
+        uint16_t  csfo;                 /* current set features options
+                                         * 15:4   reserved
+                                         * 3   auto reassign
+                                         * 2   reverting
+                                         * 1   read-look-ahead
+                                         * 0   write cache */
+        uint16_t words130_155[26];      /* reserved vendor words 130-155 */
+        uint16_t word156;
+        uint16_t words157_159[3];       /* reserved vendor words 157-159 */
+        uint16_t cfa;                   /* CFA Power mode 1 */
+        uint16_t words161_175[15];      /* Reserved */
+        uint8_t media_serial[60];       /* words 176-205 Current Media serial number */
+        uint16_t sct_cmd_transport;     /* SCT Command Transport */
+        uint16_t words207_208[2];       /* reserved */
+        uint16_t block_align;           /* Alignement of logical blocks in larger physical blocks */
+        uint32_t WRV_sec_count;         /* Write-Read-Verify sector count mode 3 only */
+        uint32_t verf_sec_count;        /* Verify Sector count mode 2 only */
+        uint16_t nv_cache_capability;   /* NV Cache capabilities */
+        uint16_t nv_cache_sz;           /* NV Cache size in logical blocks */
+        uint16_t nv_cache_sz2;          /* NV Cache size in logical blocks */
+        uint16_t rotation_rate;         /* Nominal media rotation rate */
+        uint16_t word218;               /* Reserved  */
+        uint16_t nv_cache_options;      /* NV Cache options */
+        uint16_t words220_221[2];       /* reserved */
+        uint16_t transport_major_rev;
+        uint16_t transport_minor_rev;
+        uint16_t words224_233[10];      /* Reserved */
+        uint16_t min_dwnload_blocks;    /* Minimum number of 512 byte units per DOWNLOAD MICROCODE command for mode 03h */
+        uint16_t max_dwnload_blocks;    /* Maximum number of 512 byte units per DOWNLOAD MICROCODE command for mode 03h */
+        uint16_t words236_254[19];      /* Reserved */
+        uint16_t integrity;             /* Cheksum, Signature */
+    } __attribute__((packed));
+ */
+
+//damit host to device nachrichten senden kann, muss host to device fis als struktur existieren
+
+/*
+struct FisRegisterHostToDevice {
+        // DWORD 0
+        FisType type;
+
+        uint8_t portMultiplierPort: 4;
+        uint8_t reserved1: 3;
+        uint8_t commandControl: 1;
+
+        uint8_t command;
+        uint8_t featureLow;
+
+        // DWORD 1
+        uint8_t lba0;
+        uint8_t lba1;
+        uint8_t lba2;
+        uint8_t device;
+
+        // DWORD 2
+        uint8_t lba3;
+        uint8_t lba4;
+        uint8_t lba5;
+        uint8_t featureHigh;
+
+        // DWORD 3
+        uint8_t countLow;
+        uint8_t countHigh;
+        uint8_t isochronousCommandCompletion;
+        uint8_t control;
+
+        // DWORD 4
+        uint32_t reserved2;
+    } attribute((packed));
+ */
+
 
 #[allow(warnings)]
 pub fn init(){
@@ -700,26 +861,77 @@ impl AhciController {
     return info;
     }*/
 
+    //vorher muss die read from device implementiert werden:
+    /*
+    void* AhciController::readFromDevice(uint32_t portNumber, uint32_t byteCount, const uint8_t commandFis[64], const uint8_t atapiCommand[16]) {
+    auto &memoryService = Kernel::Service::getService<Kernel::MemoryService>();
+    auto &port = registers->ports[portNumber];
+    auto commandList = virtualCommandLists[portNumber];
+
+    portLocks[portNumber].acquire();
+
+    if (!port.isActive()) {
+        portLocks[portNumber].release();
+        return nullptr;
+    }
+
+    auto slot = findCommandSlot(portNumber);
+    if (slot == UINT32_MAX) {
+        portLocks[portNumber].release();
+        return nullptr;
+    }
+
+    autodmaBuffer = allocateDmaBuffer(byteCount);
+    auto physicalDmaAddress = memoryService.getPhysicalAddress(dmaBuffer);
+
+    autocommandTable = HbaCommandTable::createCommandTable(byteCount, physicalDmaAddress);
+    Util::Address(commandTable->commandFis).copyRange(Util::Address(commandFis), sizeof(HbaCommandTable::commandFis));
+    Util::Address(commandTable->atapiCommand).copyRange(Util::Address(atapiCommand), sizeof(HbaCommandTable::atapiCommand));
+
+    auto &commandHeader = commandList[slot];
+    commandHeader.clear();
+    commandHeader.physicalRegionDescriptorTableLength = byteCount % BYTES_PER_DESCRIPTOR_ENTRY == 0 ? (byteCount / BYTES_PER_DESCRIPTOR_ENTRY) : (byteCount / BYTES_PER_DESCRIPTOR_ENTRY) + 1;
+    commandHeader.commandFisLength = sizeof(FisRegisterHostToDevice) / sizeof(uint32_t);
+    commandHeader.commandTableDescriptorBaseAddress = reinterpret_cast<uint32_t>(memoryService.getPhysicalAddress(commandTable));
+    commandHeader.atapi = atapiCommand[0] == 0 ? 0 : 1;
+
+    // Issue command
+    if (!port.issueCommand(slot)) {
+        portLocks[portNumber].release();
+        delete reinterpret_cast<uint8_t*>(dmaBuffer);
+        delete commandTable;
+        return nullptr;
+    }
+
+    portLocks[portNumber].release();
+    delete commandTable;
+    return dmaBuffer;
+}
+     */
+
 
 }
 
 // Todo:
 //Comand Liste anschauen (es werden 31 command slots unterstützt) (es wird kein weiterer gefunden)
+//command table mit allen 32 headern versuchen zu allocaten
 
-
-//tock registers (anschauen) (passt nicht)
 
 //prdt mappen und genauer anschauen:
 //  das Feld prdt, welches aktuell noch zusammen ist, muss auf 8 begrenzt werden (fertig)
 
 //command table mit Werten befülen / mapping testen (command table ist zu groß und unbestimmt, als dass sie mit Werten gefüllt werden kann. aktuell ist die prdtl = 1)
 
-//command table mit allen 32 headern versuchen zu allocaten
+
 
 // Warum wird im HHU OS ein Fehler mit F zugeschrieben, als reset?
 // Warum bekomme ich viele Ports mit der selben Adresse? gibt es nur einen Port, oder woran liegt das?
 
 //device erkennung impl
+// host to device fis struct impl
+//  device info Struct machen
+//  read from device impl
+// verstehen, wie man von read from device in das struct kommt
 
 
 
