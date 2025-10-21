1d7a525767a10153ec2b03338308f13af3062f34
FabianGremme <fagre101@hhu.de>
2025-10-21 16:47:19 +0000

    erster benchmark macht komische Werte, mal läuft es mal nicht

--- os/kernel/src/memory/ahciController.rs
+++ os/kernel/src/memory/ahciController.rs
@@ -383,8 +383,11 @@ pub fn init() {
 
         //ahci_controller.test_read(1, 1);
         //ahci_controller.test_write(1, 1);
-        // bei zu hoher Sektorgroesse macht der Speicher nicht mehr mit...
-        ahci_controller.benchmark_read(9000, 10);
+        
+        // es schafft 40000 zu lesen, aber bei 10000 nicht? je 10 reps
+        // bei 20 reps ist die shell nicht mehr da???
+        ahci_controller.benchmark_read(400, 100);
+        
     }
     //die GHCR sind in Section 3 der Spezifikation zu finden. ich weiß noch nicht, wie man bis dahin kommt
 }
