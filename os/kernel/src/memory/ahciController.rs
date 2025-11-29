13e2df8fe51b420d04efeb3b881f81e18b3a722c
FabianGremme <fagre101@hhu.de>
2025-11-29 14:42:18 +0000

    starte experimente mit den registern

--- os/kernel/src/memory/ahciController.rs
+++ os/kernel/src/memory/ahciController.rs
@@ -453,7 +453,7 @@ pub fn init() {
         ahci_controller.benchmark_write(1024 * 100, 100, 0);
 
         ahci_controller.benchmark_read(1024 * 1024 * 1, 100, 0);
-        ahci_controller.benchmark_write(1024 * 1024 * 1, 100, 0);*///
+        ahci_controller.benchmark_write(1024 * 1024 * 1, 100, 0);*/
 
 
         //ahci_controller.benchmark_random_read(5, 1);
